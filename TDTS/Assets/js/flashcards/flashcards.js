1e602b8a5f724253af5c0c942f74b28ddb2535a1
lg2game <102352533+LG2GAME@users.noreply.github.com>
2024-01-02 20:42:43 +0100

    Add files via upload

---
+++ TDTS/Assets/js/flashcards/flashcards.js
@@ -0,0 +1,66 @@
+let flashcards = [
+    {
+        id: "0",
+        concept: "Bezpieczeństwo w sieci!",
+        img: "",
+        definition: "Bezpieczeństwo w sieci polega na ochronie danych przed nieuprawnionym dostępem.",
+        note: "Aby zapewnić ochronę swojego komputera i telefonu, należy uważać na strony, z których korzystamy. Aby upewnić się, że nikt nie ma dostępu do naszego urządzenia, należy zainstalować systemy antywirusowe, wykrywania włamań."
+    },
+    {
+        id: "1",
+        concept: "Phishing",
+        img: "",
+        definition: "Rodzaj oszustwa internetowego, w którym cyberprzestępcy podszywają się pod inną osobę w celu wyłudzenia poufnych informacji.",
+        note: "Aby się chronić przed atakami phishingowymi, należy ostrożnie wchodzić w linki, podane w mailach lub SMS-ach. Uważać na wiadomości, które zawierają błędy ortograficzne lub literówki. Nigdy nie podawać poufnych informacji osobom, których nie znasz."
+    },
+    {
+        id: "2",
+        concept: "Malware - złosliwe oprogramowanie ",
+        img: "",
+        definition: "Czyli złośliwe oprogramowania. Jego celem jest zdobycie dostępu do twojego komputera lub informacji i danych na twój temat",
+        note: "Aby chronić się przed wirusami, możesz utrzymać swoje oprogramowanie antywirusowe i zapory aktualne. Zawsze być ostrożnym przy pobieraniu plików i unikać instalacji z podejrzanych linków. W razie potrzeby zawsze miej najnowsze kopie zapasowe swoich plików."
+    },
+    {
+        id: "3",
+        concept: "Ransomware – blokada urządzenia",
+        img: "",
+        definition: "Ransomware to rodzaj wirusa, który blokuje dostęp do komputera lub plików do momentu zapłacenia okupu.",
+        note: "Aby zapobiegać tego tupu atakowi, pamiętaj aby unikać podejrzanych linków, zachowaj ostrożność przy pobieraniu plików z internetu, pamiętaj aby posiadać aktualne oprogramowanie antywirusowe."
+    },
+
+    {
+        id: "4",
+        concept: "Zagrożenia dla prywatności",
+        img: "",
+        definition: "To wszelkie działania, które mogą doprowadzić do ujawnienia lub naruszenia danych osobowych",
+        note: "Aby chronić swoją prywatność w sieci, uważaj komu udostępniasz informacje o sobie, nigdy nie używaj tych samych haseł. Aktualizuj oprogramowania antywirusowe i antyspamowe. Używaj bezpiecznego połączenia internetowego i monitoruj swoją aktywność w sieci."
+    },
+    {
+        id: "5",
+        concept: "Kradzież danych",
+        img: "",
+        definition: "Cyberprzestępcy włamują się do baz danych, by wykradzionymi danymi użytkowników później manipulować.",
+        note: "Aby chronić się przed kradzieżami danych pamiętaj aby używać innych haseł w innych serwisach - utrudni to potencjalne wykradanie danych. Włącz dwuskładnikowe uwierzytelnianie (2FA)."
+    },
+    {
+        id: "6",
+        concept: "Kradzież tożsamości",
+        img: "",
+        definition: "",
+        note: "Kradzież tożsamości to kolejne poważne zagrożenie w sieci. Cyberprzestępcy mogą wykorzystać wykradzione lub zdobyte w inny sposób informacje takie jak numer PESEL, skan dowodu osobistego, adres czy data urodzenia do kradzieży Twojej tożsamości. Takie działanie może prowadzić do wielu negatywnych konsekwencji, na przykład zaciągania pożyczek na Twoje dane."
+    },
+    {
+        id: "7",
+        concept: "Cyberprzemoc – nękanie w sieci",
+        img: "",
+        definition: "",
+        note: "Cyberprzemoc to zjawisko polegające na wykorzystywaniu technologii do nękania, zastraszania lub ośmieszania innych. Może to obejmować takie działania jak doxxing (udostępnianie prywatnych informacji o osobie w sieci), hejt, wysyłanie obraźliwych wiadomości, nękanie psychiczne przez internet, publikowanie fałszywych informacji o innych osobach w sieci czy groźby przemocy fizycznej."
+    },
+    {
+        id: "",
+        concept: "",
+        img: "",
+        definition: "",
+        note: ""
+    },
+]
